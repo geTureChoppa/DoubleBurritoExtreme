664443caf93d5126ab721e28d7ff7606bf085861
geTureChoppa <jamesapplejuice@gmail.com>
2022-04-14 16:31:40 +1200

    DID STUFF

---
+++ src/doubleburritoextreme/DoubleBurritoExtreme.java
@@ -0,0 +1,23 @@
+package doubleburritoextreme;
+import java.util.Timer;
+
+public class DoubleBurritoExtreme {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws InterruptedException {
+        //Great Speech
+        for(int i=0;i<99;i++)
+        {
+            System.out.print("I'm a millionaire self-taught investor "
+                + "that's sharing all my research, investment "
+                + "principles, and trade ideas in a free newsletter. ");
+            
+        }
+
+        Thread.sleep(3000);
+        System.out.println("\n\n(its funnier if you make the console Wrap Text)");
+    }
+    
+}
